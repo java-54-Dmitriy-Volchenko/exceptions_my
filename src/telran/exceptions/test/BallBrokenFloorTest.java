2449836ef24effaecdd0b1fd6e7917c3d8617891
DmitriyVolchenko <Dmitriy.Volchenko@gmail.com>
2024-04-02 19:40:59 +0300

    makes method getMinBrokenFloor

---
+++ src/telran/exceptions/test/BallBrokenFloorTest.java
@@ -0,0 +1,37 @@
+package telran.exceptions.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import telran.exceptions.BallBrokenFloor;
+
+class BallBrokenFloorTest {
+
+	private static final int N_FLOORS = 100;
+
+	@Test
+	void ballBrokenFloorTest() {
+		BallBrokenFloor bbf = new BallBrokenFloor(N_FLOORS);
+		assertEquals(bbf.getBrokenFloor(), getMinBrokenFloor(bbf));
+	}
+
+	private int getMinBrokenFloor(BallBrokenFloor bbf) {
+		    int left = 1;
+		    int right = N_FLOORS;
+
+		    while (left <= right) {
+		        int middle = left/2 + right/2; 
+
+		        try {
+		            bbf.checkFloor(middle);
+		            left = middle + 1; 
+		        } catch (Exception e) {
+		            right = middle - 1; 
+		        }
+		    }
+
+		    return left; 
+	}
+
+}
